77395074bb6fdc5e0bb067629d8fe844820783bc
schwarzalexey <97682066+schwarzalexey@users.noreply.github.com>
2023-04-15 12:10:47 +0300

    Add files via upload

--- GameJam/game.py
+++ GameJam/game.py
@@ -1,9 +1,7 @@
-import random
-
 import pygame
 import os
 from const import *
-
+import random
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('sprites', name)
@@ -63,7 +61,15 @@ class Base(pygame.sprite.Sprite):
         self.image.blit(pygame.transform.scale(load_image("bee_base.jpg"), (75, 75)), (0, 0, 75, 75))
         self.rect = self.image.get_rect()
         self.rect.x = screen_size[0] - 85
-        self.rect.y = screen_size[1] - 85
+        self.rect.y = 85
+        self.nectar = 100
+        self.health = 100
+
+    def update(self):
+        if self.nectar > 0:
+            self.nectar = max(0, self.nectar - 3/FPS)
+        else:
+            self.health = max(0, self.health - 5/FPS)
 
 
 class Flower(pygame.sprite.Sprite):
@@ -82,21 +88,24 @@ class Flower(pygame.sprite.Sprite):
 
 def Game(screen):
     running = True
+    base = Base()
     base_sprite = pygame.sprite.Group()
-    base_sprite.add(Base())
+    base_sprite.add()
     bee = Bee((WIDTH // 2, HEIGHT // 2), "bee.png")
     player = pygame.sprite.Group()
     player.add(bee)
-
+    font = pygame.font.Font("fonts/Font.ttf", 24)
+    health = font.render("Health: 100", True, (255, 0, 0))
+    health_rect = health.get_rect()
+    health_rect.x, health_rect.y = 0, 0
+    screen.blit(health, health_rect)
     flower_sprites = pygame.sprite.Group()
-
     for i in range(5):
-        flower_sprites.add(Flower((50+i*200, 600)))
-    
+        flower_sprites.add(Flower((50 + i * 200, 600)))
 
     while running:
         clock.tick(FPS)
-
+        print(base.nectar)
         keys = pygame.key.get_pressed()
 
         for event in pygame.event.get():
@@ -120,20 +129,38 @@ def Game(screen):
                 bee.rect.y = c.rect.y
             else:
                 bee.is_flower = False
+            bee.nectar += max(0, c.wnektar - 2 / FPS)
 
-            bee.nectar+=max(0, c.self.nektar-2/FPS)
+        nest_collide = pygame.sprite.spritecollide(bee, base_sprite, False)
 
-
-
-
-
-
+        if nest_collide:
+            if nest_collide[0].nectar != 100:
+                if min(nest_collide[0].nectar + 5/FPS, 100) == 100:
+                    bee.nectar = max(bee.nectar - 100 - nest_collide[0].nectar, 0)
+                    nest_collide[0].nectar = 100
+                else:
+                    bee.nectar = max(bee.nectar - 5/FPS, 0)
+                    nest_collide[0].nectar += 5/FPS
 
         screen.fill(BLACK)
+
+        health = font.render(f"Health: {round(base.health)}", True, (255, 0, 0))
+        health_rect = health.get_rect()
+        health_rect.x, health_rect.y = 0, 0
+        screen.blit(health, health_rect)
+
+        nectar = font.render(f"Nectar: {round(base.nectar)}", True, (255, 255, 0))
+        nectar_rect = nectar.get_rect()
+        nectar_rect.x, nectar_rect.y = 0, 50
+        screen.blit(nectar, nectar_rect)
+
+        bee_nectar = global_font_15.render(f"{round(bee.nectar)}", True, (int(255 * (bee.nectar/25)), int(255 * (1-bee.nectar/25)), 0))
+        bee_nectar_rect = bee_nectar.get_rect(center=(bee.rect.x + 25, bee.rect.y - 10))
+        screen.blit(bee_nectar, bee_nectar_rect)
+
         player.update()
-        base_sprite.draw(screen)
-
+        base.update()
         flower_sprites.draw(screen)
-
         player.draw(screen)
+        base_sprite.draw(screen)
         pygame.display.flip()
