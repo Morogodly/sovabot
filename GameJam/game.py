71a83876e6f2c0aa77ad233f18e6f3d388a8993c
Morogodly <81529175+Morogodly@users.noreply.github.com>
2023-04-15 10:25:50 +0300

    Add files via upload

--- GameJam/game.py
+++ GameJam/game.py
@@ -3,30 +3,35 @@ import pygame
 import const
 from const import *
 
-class Bee():
-    def __init__(self, pos, image):
-        self.x = pos[0]
-        self.y = pos[1]
-        self.image = pygame.image.load(image)
+class Bee(pygame.sprite.Sprite):
+    def __init__(self,pos, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
+        self.image.convert_alpha()
+        self.image.blit(pygame.image.load(image), (0, 0, 50, 50))
+        self.rect = self.image.get_rect()
         self.x_speed = 8
         self.y_speed = 8
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
     def x_motion(self, k_right):
-        if 0<self.x<WIDTH:
+        if self.rect.x <= WIDTH:
             if k_right:
-                self.x += self.x_speed
-            else:
-                self.x -= self.y_speed
+                self.rect.x+=self.x_speed
+        if self.rect.x >= 0:
+            if not k_right:
+                self.rect.x-=self.x_speed
 
     def y_motion(self, k_up):
-        if 0<self.y<HEIGHT:
+        if self.rect.y <= HEIGHT:
+            if not k_up:
+                self.rect.y += self.y_speed
+        if self.rect.y >= 0:
             if k_up:
-                self.y -= self.y_speed
-            else:
-                self.y += self.x_speed
+                self.rect.y -= self.y_speed
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+
 
 
 
@@ -36,20 +41,32 @@ class Bee():
 def Game(screen):
     running = True
 
-    player = Bee((WIDTH//2, HEIGHT//2), "menu_buttons\ok.png")
+    bee = Bee((WIDTH//2, HEIGHT//2),"menu_buttons\ok.png")
+    player = pygame.sprite.Group()
+    player.add(bee)
 
 
     while running:
         clock.tick(FPS)
 
+        keys = pygame.key.get_pressed()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    player.x_motion(True)
+        if keys[pygame.K_d]:
+            bee.x_motion(k_right=True)
+        elif keys[pygame.K_a]:
+            bee.x_motion(k_right=False)
+        if keys[pygame.K_w]:
+            bee.y_motion(True)
+        else:
+            bee.y_motion(False)
+
+
 
         screen.fill(BLACK)
+        player.update()
         player.draw(screen)
         pygame.display.flip()
